705fdd4d4f59bcc442419b142ed28e9e329cec46
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-13 03:08:32 -0700

    3.11 showing CreatedOn date

--- app.js
+++ app.js
@@ -13,9 +13,12 @@
     };
   });
 
-  app.controller('ReviewController', function(){
+ app.controller("ReviewController", function(){
+
     this.review = {};
+
     this.addReview = function(product){
+      this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
     };
